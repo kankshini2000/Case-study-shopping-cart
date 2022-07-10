620fe6b37ab8fdf440647b3f2b19eae4969f8189
kankshini2000 <84704527+kankshini2000@users.noreply.github.com>
2022-07-10 11:20:53 +0530

    Delete order directory

--- order/src/main/java/com/shop/order/exception/OrderNotFound.java
+++
@@ -1,9 +0,0 @@
-package com.shop.order.exception;
-
-public class OrderNotFound extends Exception{
-    private static final long serialVersionUID = 1L;
-
-	public OrderNotFound(String msg){
-        super(msg);
-    }
-}
