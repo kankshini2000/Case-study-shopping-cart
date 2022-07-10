9df5511f0ae1b765cfa2b19a545c7bbf78337be0
kankshini2000 <84704527+kankshini2000@users.noreply.github.com>
2022-07-10 11:20:36 +0530

    Add files via upload

---
+++ cart/src/main/java/com/shop/cart/security/CartSecurity.java
@@ -0,0 +1,35 @@
+package com.shop.cart.security;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class CartSecurity extends WebSecurityConfigurerAdapter{
+
+    @Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("kanshi").password("kanshi").roles("USER");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.httpBasic().and().authorizeRequests().antMatchers("/cart/add", "/cart/update/**","/cart/getAll","/cart/delete/**").permitAll()
+         //	     .antMatchers("/user/all,/user/**").permitAll()
+				.and().csrf().disable().headers().frameOptions()
+				.disable();
+		// .formLogin();
+	}
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+    
+}
