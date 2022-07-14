a3d571810ea34127097ddfd8c7b46cbc8f7e4d0f
kankshini2000 <84704527+kankshini2000@users.noreply.github.com>
2022-07-14 14:07:11 +0530

    Delete cart directory

--- cart/src/main/java/com/shop/cart/exception/CartNotFound.java
+++
@@ -1,9 +0,0 @@
-package com.shop.cart.exception;
-
-public class CartNotFound extends Exception{
-    private static final long serialVersionUID = 1L;
-
-    public CartNotFound(String msg){
-        super(msg);
-    }
-}
