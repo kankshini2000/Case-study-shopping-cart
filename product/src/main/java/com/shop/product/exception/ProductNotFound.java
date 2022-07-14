add1176196dc0244cbe2fdd55bef9715c6b3ee60
kankshini2000 <84704527+kankshini2000@users.noreply.github.com>
2022-07-14 14:10:22 +0530

    Delete product directory

--- product/src/main/java/com/shop/product/exception/ProductNotFound.java
+++
@@ -1,9 +0,0 @@
-package com.shop.product.exception;
-
-public class ProductNotFound extends Exception{
-    private static final long serialVersionUID = 1L;
-
-	public ProductNotFound(String msg) {
-		super(msg);
-	}
-}
